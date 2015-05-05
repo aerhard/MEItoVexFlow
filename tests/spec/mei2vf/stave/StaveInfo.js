4e28996ef6d95c213f0ca96270dcf5a50a35d702
aerhard <a_erhard@gmx.de>
2015-05-05 09:25:59 +0200

    fixed test conditions; renamed variable names

--- tests/spec/mei2vf/stave/StaveInfo.js
+++ tests/spec/mei2vf/stave/StaveInfo.js
@@ -87,7 +87,7 @@ define([
 
     describe("updateDef", function () {
 
-      it('doesn\t override specifications if no MEI elements have been passed', function () {
+      it('doesn\'t override specifications if no MEI elements have been passed', function () {
 
         var staffDef = mei('<staffDef/>');
         var scoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
@@ -110,108 +110,108 @@ define([
 
       });
 
-      it('doesn\t override specifications if there are no relevant specifications in the elements passed', function () {
+      it('doesn\'t override specifications if there are no relevant specifications in the elements passed', function () {
 
-        var staffDef = mei('<staffDef/>');
-        var scoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef/>');
+        var initialScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, false, false, false);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, false, false, false);
 
-        var staffDef1 = mei('<staffDef/>');
-        var scoreDef1 = mei('<scoreDef/>');
+        var newStaffDef = mei('<staffDef/>');
+        var newScoreDef = mei('<scoreDef/>');
 
-        staveInfo.updateDef(staffDef1, scoreDef1);
+        staveInfo.updateDef(newStaffDef, newScoreDef);
 
         expect(staveInfo.keySpec.key).toEqual('B');
-        expect(staveInfo.keySpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.keySpec.meiElement).toEqual(initialScoreDef);
 
         expect(staveInfo.timeSpec.count).toEqual(2);
         expect(staveInfo.timeSpec.unit).toEqual(2);
         expect(staveInfo.timeSpec.sym).toEqual('C');
-        expect(staveInfo.timeSpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.timeSpec.meiElement).toEqual(initialScoreDef);
 
         expect(staveInfo.clef.type).toEqual('bass');
         expect(staveInfo.clef.shift).toEqual(-1);
-        expect(staveInfo.clef.meiElement).toEqual(scoreDef);
+        expect(staveInfo.clef.meiElement).toEqual(initialScoreDef);
 
       });
 
 
       it('overrides with specifications in <staffDef> if present', function () {
 
-        var staffDef = mei('<staffDef/>');
-        var scoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef/>');
+        var initialScoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, false, false, false);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, false, false, false);
 
-        var staffDef1 = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
-        var scoreDef1 = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var newStaffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
+        var newScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        staveInfo.updateDef(staffDef1, scoreDef1);
+        staveInfo.updateDef(newStaffDef, newScoreDef);
 
         expect(staveInfo.keySpec.key).toEqual('Am');
-        expect(staveInfo.keySpec.meiElement).toEqual(staffDef);
+        expect(staveInfo.keySpec.meiElement).toEqual(newStaffDef);
 
         expect(staveInfo.timeSpec.count).toEqual(4);
         expect(staveInfo.timeSpec.unit).toEqual(4);
         expect(staveInfo.timeSpec.sym).toBeNull();
-        expect(staveInfo.timeSpec.meiElement).toEqual(staffDef);
+        expect(staveInfo.timeSpec.meiElement).toEqual(newStaffDef);
 
         expect(staveInfo.clef.type).toEqual('treble');
         expect(staveInfo.clef.shift).toBeUndefined();
-        expect(staveInfo.clef.meiElement).toEqual(staffDef);
+        expect(staveInfo.clef.meiElement).toEqual(newStaffDef);
 
       });
 
       it('overrides with specifications in <scoreDef> if not present in <staffDef>', function () {
 
-        var staffDef = mei('<staffDef/>');
-        var scoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef/>');
+        var initialScoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, false, false, false);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, false, false, false);
 
-        var staffDef1 = mei('<staffDef/>');
-        var scoreDef1 = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var newStaffDef = mei('<staffDef/>');
+        var newScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        staveInfo.updateDef(staffDef1, scoreDef1);
+        staveInfo.updateDef(newStaffDef, newScoreDef);
 
         expect(staveInfo.keySpec.key).toEqual('B');
-        expect(staveInfo.keySpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.keySpec.meiElement).toEqual(newScoreDef);
 
         expect(staveInfo.timeSpec.count).toEqual(2);
         expect(staveInfo.timeSpec.unit).toEqual(2);
         expect(staveInfo.timeSpec.sym).toEqual('C');
-        expect(staveInfo.timeSpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.timeSpec.meiElement).toEqual(newScoreDef);
 
         expect(staveInfo.clef.type).toEqual('bass');
         expect(staveInfo.clef.shift).toEqual(-1);
-        expect(staveInfo.clef.meiElement).toEqual(scoreDef);
+        expect(staveInfo.clef.meiElement).toEqual(newScoreDef);
 
       });
 
 
       it('overrides with specifications in <scoreDef> if <staffDef> is undefined', function () {
 
-        var staffDef = mei('<staffDef/>');
-        var scoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef/>');
+        var initialScoreDef = mei('<scoreDef key.pname="c" key.mode="major" meter.count="3" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="3" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, false, false, false);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, false, false, false);
 
-        var scoreDef1 = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var newScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        staveInfo.updateDef(undefined, scoreDef1);
+        staveInfo.updateDef(undefined, newScoreDef);
 
         expect(staveInfo.keySpec.key).toEqual('B');
-        expect(staveInfo.keySpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.keySpec.meiElement).toEqual(newScoreDef);
 
         expect(staveInfo.timeSpec.count).toEqual(2);
         expect(staveInfo.timeSpec.unit).toEqual(2);
         expect(staveInfo.timeSpec.sym).toEqual('C');
-        expect(staveInfo.timeSpec.meiElement).toEqual(scoreDef);
+        expect(staveInfo.timeSpec.meiElement).toEqual(newScoreDef);
 
         expect(staveInfo.clef.type).toEqual('bass');
         expect(staveInfo.clef.shift).toEqual(-1);
-        expect(staveInfo.clef.meiElement).toEqual(scoreDef);
+        expect(staveInfo.clef.meiElement).toEqual(newScoreDef);
 
       });
 
@@ -242,18 +242,18 @@ define([
 
       it('should set renderWith.clef to true if the current clef is defined by a <clef> and the new staff definition has diverging properties ', function () {
 
-        var staffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
-        var scoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
+        var initialScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, true, true, true);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, true, true, true);
 
         var clef = mei('<clef shape="C" line="4"/>');
 
         staveInfo.clefChangeInMeasure(clef);
 
-        var staffDef1 = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
-        var scoreDef1 = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
-        staveInfo.updateDef(staffDef1, scoreDef1);
+        var newStaffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
+        var newScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        staveInfo.updateDef(newStaffDef, newScoreDef);
 
         expect(staveInfo.renderWith.clef).toEqual(true);
 
@@ -261,18 +261,18 @@ define([
 
       it('should set renderWith.clef to false if the current clef is defined by a <clef> and the new staff definition has corresponding properties ', function () {
 
-        var staffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
-        var scoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
+        var initialStaffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="G" clef.line="2"/>');
+        var initialScoreDef = mei('<scoreDef key.pname="b" key.mode="major" meter.count="2" meter.unit="2" meter.sym="C" clef.shape="F" clef.line="4" clef.dis="8" clef.dis.place="below"/>');
 
-        var staveInfo = new StaveInfo(staffDef, scoreDef, true, true, true);
+        var staveInfo = new StaveInfo(initialStaffDef, initialScoreDef, true, true, true);
 
         var clef = mei('<clef shape="C" line="4"/>');
 
         staveInfo.clefChangeInMeasure(clef);
 
-        var staffDef1 = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="C" clef.line="4"/>');
+        var newStaffDef = mei('<staffDef key.pname="a" key.mode="minor" meter.count="4" meter.unit="4" clef.shape="C" clef.line="4"/>');
 
-        staveInfo.updateDef(staffDef1, scoreDef);
+        staveInfo.updateDef(newStaffDef, initialScoreDef);
 
         expect(staveInfo.renderWith.clef).toEqual(false);
 
