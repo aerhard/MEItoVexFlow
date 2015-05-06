2c4068d3eea64ee7879b5deb43272a3de1c2cf80
aerhard <a_erhard@gmx.de>
2015-05-06 15:45:56 +0200

    adjusted indentation in new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   'use strict';
 
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
 
           optimize: "none",
 
-          done: function(done, output) {
+          done: function (done, output) {
             var duplicates = require('rjs-build-analysis').duplicates(output);
             if (duplicates.length > 0) {
               grunt.log.subhead('Duplicates found in requirejs build:');
@@ -43,40 +43,40 @@ module.exports = function(grunt) {
           },
 
           // based on jQuery's convert function, see [https://github.com/jquery/jquery/blob/master/build/tasks/build.js]
-          onBuildWrite: function( name, path, contents ) {
+          onBuildWrite: function (name, path, contents) {
             var rdefineEnd = /\}\);[^}\w]*$/;
             var amdName;
             // Convert var modules
-            if ( /.\/var\//.test( path ) ) {
+            if (/.\/var\//.test(path)) {
               contents = contents
-                .replace( /define\([\w\W]*?return/, "var " + (/var\/([\w-]+)/.exec(name)[1]) + " =" )
-                .replace( rdefineEnd, "" );
+                .replace(/define\([\w\W]*?return/, "var " + (/var\/([\w-]+)/.exec(name)[1]) + " =")
+                .replace(rdefineEnd, "");
 
             } else {
 
               // Ignore jQuery's exports (the only necessary one)
-              if ( name !== "jquery" ) {
+              if (name !== "jquery") {
                 contents = contents
-                  .replace( /\s*return\s+[^\}]+(\}\);[^\w\}]*)$/, "$1" )
+                  .replace(/\s*return\s+[^\}]+(\}\);[^\w\}]*)$/, "$1")
                   // Multiple exports
-                  .replace( /\s*exports\.\w+\s*=\s*\w+;/g, "" );
+                  .replace(/\s*exports\.\w+\s*=\s*\w+;/g, "");
               }
 
               // Remove define wrappers, closure ends, and empty declarations
               contents = contents
-                .replace( /define\([^{]*?{/, "" )
-                .replace( rdefineEnd, "" );
+                .replace(/define\([^{]*?{/, "")
+                .replace(rdefineEnd, "");
 
               // Remove anything wrapped with
               // /* ExcludeStart */ /* ExcludeEnd */
               // or a single line directly after a // BuildExclude comment
               contents = contents
-                .replace( /\/\*\s*ExcludeStart\s*\*\/[\w\W]*?\/\*\s*ExcludeEnd\s*\*\//ig, "" )
-                .replace( /\/\/\s*BuildExclude\n\r?[\w\W]*?\n\r?/ig, "" );
+                .replace(/\/\*\s*ExcludeStart\s*\*\/[\w\W]*?\/\*\s*ExcludeEnd\s*\*\//ig, "")
+                .replace(/\/\/\s*BuildExclude\n\r?[\w\W]*?\n\r?/ig, "");
 
               // Remove empty definitions
               contents = contents
-                .replace( /define\(\[[^\]]+\]\)[\W\n]+$/, "" );
+                .replace(/define\(\[[^\]]+\]\)[\W\n]+$/, "");
             }
             return contents;
           }
@@ -115,37 +115,37 @@ module.exports = function(grunt) {
     },
 
     jasmine: {
-        testdev: {
-            src: 'src/*/*.js',
-            options: {
-                specs: 'tests/spec/*/*.js',
-                helpers: ['tests/loadXMLDoc.js', 'tests/phantomPolyFill.js'],
-                host: 'http://127.0.0.1:8000/',
-                template: require('grunt-template-jasmine-requirejs'),
-                options : {
-                    keepRunner :true
-                },
-                templateOptions: {
-                    requireConfig: {
-                        baseUrl: 'src/',
-                        paths : {
-                            'tests' : '../tests',
-                                'jquery' : '../bower_components/jquery/dist/jquery.min',
-                                'vex' : '../bower_components/vexflow/releases/vexflow-min',
-                            'vexflow':'../src/mei2vf/vexflow',
-                                'common' : '../src/common',
-                                'mei2vf' : '../src/mei2vf',
-                                'meilib' : '../src/meilib'
-                        },
-                        shim : {
-                            'vex' : {
-                                exports : 'Vex'
-                            }
-                        }
-                    }
+      testdev: {
+        src: 'src/*/*.js',
+        options: {
+          specs: 'tests/spec/*/*.js',
+          helpers: ['tests/loadXMLDoc.js', 'tests/phantomPolyFill.js'],
+          host: 'http://127.0.0.1:8000/',
+          template: require('grunt-template-jasmine-requirejs'),
+          options: {
+            keepRunner: true
+          },
+          templateOptions: {
+            requireConfig: {
+              baseUrl: 'src/',
+              paths: {
+                'tests': '../tests',
+                'jquery': '../bower_components/jquery/dist/jquery.min',
+                'vex': '../bower_components/vexflow/releases/vexflow-min',
+                'vexflow': '../src/mei2vf/vexflow',
+                'common': '../src/common',
+                'mei2vf': '../src/mei2vf',
+                'meilib': '../src/meilib'
+              },
+              shim: {
+                'vex': {
+                  exports: 'Vex'
                 }
+              }
             }
+          }
         }
+      }
     }
 
   });
@@ -166,4 +166,4 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['dist']);
 
-}
+};
